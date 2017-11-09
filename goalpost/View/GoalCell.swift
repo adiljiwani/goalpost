6c8dcc97bfdf03a7d5741c83f152e90ae8ac4289
Adil Jiwani <a8jiwani@uwaterloo.ca>
2017-11-09 14:19:15 -0500

    created goal cell

---
+++ goalpost/View/GoalCell.swift
@@ -0,0 +1,23 @@
+//
+//  GoalCell.swift
+//  goalpost
+//
+//  Created by Adil Jiwani on 2017-11-09.
+//  Copyright © 2017 Adil Jiwani. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    
+    @IBOutlet weak var goalDescription: UILabel!
+    @IBOutlet weak var goalType: UILabel!
+    @IBOutlet weak var goalProgress: UILabel!
+    
+    func configureCell(description: String, goalType: String, goalProgressAmt: Int) {
+        self.goalDescription.text = description
+        self.goalType.text = goalType
+        self.goalProgress.text = String(describing: goalProgressAmt)
+    }
+}
